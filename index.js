b2b1cb60d53f1cfaf6ffbab9de4894110ac7d408
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-09 13:26:03 +0100

    :rocket: :tada: weather index.js  :rocket: :tada:

---
+++ index.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+const search = document.querySelector(".search-box button");
+const weatherBox = document.querySelector(".weather-box");
+const weatherDetails = document.querySelector(".weather-details");
+const error404 = document.querySelector(".not-found");
+
+search.addEventListener('click', () =>{
+    const APIKey = "6b58924fc67d6b5236f1a1d6b5721f98"
+})
