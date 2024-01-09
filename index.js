7f4d087ed761f2f3fe4c498693fbe4951fc6e104
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-09 14:27:54 +0100

    :rocket: :tada: weather index.js  :rocket: :tada:

--- index.js
+++ index.js
@@ -6,4 +6,22 @@ const error404 = document.querySelector(".not-found");
 
 search.addEventListener('click', () =>{
     const APIKey = "6b58924fc67d6b5236f1a1d6b5721f98"
+    const city = document.querySelector(".search-box input").value;
+
+    if(city === '')
+        return;
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json)
+        .then(json =>{
+            if(json.cod ==='404'){
+                container.style.height = "400px";
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display= "block";
+                error404.classList.add("fadeIn")
+                return;
+            }
+        })
+
 })
